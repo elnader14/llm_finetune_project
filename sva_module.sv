1c67c9ee9da9d8c3fcc6c1e141126b8895916fe7
elnader14 <elnader14@gmail.com>
2024-08-24 13:46:51 +0000

    Sending a file from environment

--- sva_module.sv
+++ sva_module.sv
@@ -1,3 +1,3 @@
 module sva_module (input a , input b , input s , input c ); 
-assert property (@(posedge clk) (s == 1'b0) |-> (c == (a + b)));
+assert property (@(posedge clk) (s == 0) |-> (c == (a + b)));
 module
